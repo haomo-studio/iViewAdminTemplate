54d66c067bf00da008f3f7df391ba7950194d33b
wsldiy <18612224431@163.com>
2017-03-22 17:52:12 +0800

    wednesday

--- src/views/module/cards.vue
+++ src/views/module/cards.vue
@@ -1,8 +1,143 @@
 <template>
-   卡片
+   <div id="card_done" style="background-color:#eee;">
+      <p class="card">卡片</p>
+      <div class="title_a">Cards are useful for showing excerpts, quick information or notifications. Using <a>ms-card</a> directive you can easily create different cards compatible with Google's Material Design specifications.</div>
+      <div class="title_a"><a>ms-card</a> directive templates have pre-defined content areas. The reason for that is to force you to create cards that follows both Google's Material Design specifications and Fuse Theme's general design rules. But of course you can create your own designs and templates and use them with <a>ms-card</a> directive.</div>
+      <div class="template_box">
+      <h2>Template 1</h2>
+      <Card style="width:450px">
+         <div style="width:90%;height:300px;">
+            <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+            <div><span style="font-family:Main Head;font-weight:bold;color:#464c5b;font-size:25px;">Nature</span><br>
+                 <span style="font-family:Help;font-weight:normal;color:#9ea7b4;font-size:13px;">Mountain view from lake side</span></div>
+            <div style="margin-top:20px"><span  style="font-family:Sub Head;font-weight:bold;color:#464c5b;font-size:15px;">
+               Look deep into nature, then you will understand everything better.</span></div>
+         </div>
+      </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 2</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <div><span style=""><img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:20%"></span>
+               <span style="font-family:Sub Head;font-weight:bold;color:#464c5b;font-size:13px;">Alice Freeman posted on Nature</span><br>
+                    <span style="display:block;margin-left:60px;font-family:Help;font-weight:normal;color:#9ea7b4;font-size:13px;">32 minutes ago</span></div>
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:100%;margin-top:10px;">
+               <div><span style="font-family:Sub Head;font-weight:bold;color:#464c5b;font-size:15px;">Look deep into nature, then you will understand everything better. Look deep into nature, then you will understand everything better.</span></div>
+            </div>
+         </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 3</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+               <div><span>Nature</span></div><br>
+               <span>Mountain view from lake side</span>
+               <span>Look deep into nature, then you will understand everything better.</span>
+            </div>
+         </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 4</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+               <div><span>Nature</span></div><br>
+               <span>Mountain view from lake side</span>
+               <span>Look deep into nature, then you will understand everything better.</span>
+            </div>
+         </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 5</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+               <div><span>Nature</span></div><br>
+               <span>Mountain view from lake side</span>
+               <span>Look deep into nature, then you will understand everything better.</span>
+            </div>
+         </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 6</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+               <div><span>Nature</span></div><br>
+               <span>Mountain view from lake side</span>
+               <span>Look deep into nature, then you will understand everything better.</span>
+            </div>
+         </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 7</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+               <div><span>Nature</span></div><br>
+               <span>Mountain view from lake side</span>
+               <span>Look deep into nature, then you will understand everything better.</span>
+            </div>
+         </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 8</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+               <div><span>Nature</span></div><br>
+               <span>Mountain view from lake side</span>
+               <span>Look deep into nature, then you will understand everything better.</span>
+            </div>
+         </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 9</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+               <div><span>Nature</span></div><br>
+               <span>Mountain view from lake side</span>
+               <span>Look deep into nature, then you will understand everything better.</span>
+            </div>
+         </Card>
+      </div>
+      <div class="template_box">
+         <h2>Template 10</h2>
+         <Card style="width:450px">
+            <div style="width:70%;height:300px;">
+               <img src="D:\ivue\iViewAdminTemplate\node_modules\stream-http\test\server\static/browserify.png" style="width:40%;margin-bottom:20px;">
+               <div><span>Nature</span></div><br>
+               <div><span>Mountain view from lake side</span></div>
+               <div><span>Look deep into nature, then you will understand everything better.</span></div>
+            </div>
+         </Card>
+      </div>
+   </div>
 </template>
 <script>
     export default {
 
     }
 </script>
+<style>
+   .card{
+      font-family: Help;
+      font-size: 30px;
+      color:#c3cbd6;
+      width:100%;
+      height: 150px;
+      background-color:#3399ff;
+      padding-left:10px;
+      padding-top:30px;
+   }
+   .title_a{
+      margin-top:20px;
+      margin-left:10px;
+   }
+   .template_box{
+      margin-top:40px;
+   }
+</style>
