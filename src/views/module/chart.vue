79006ed60c7fe81529c22b35e9350c70cb39cdc0
申海东 <shenhaidong@haomo-studio.com>
2017-03-23 12:11:08 +0800

    pie-echart

--- src/views/module/chart.vue
+++ src/views/module/chart.vue
@@ -2,87 +2,129 @@
   <div style="padding: 20px">
     <Card :bordered="false">
       <p slot="title">散点图</p>
-      <div id="main" v-model = 'option'  :style="{width:'600px',height:'400px'}"></div>
+      <div id="main"  :style="{width:'600px',height:'400px'}"></div>
     </Card>
   </div>
 </template>
 <script>
 
+  import echarts from 'echarts';
+  import $ from 'jquery';
   export default {
     data () {
-      return{
-        option:{
-          title: {
-            text: '折线图堆叠'
-          }
-          ,
-          tooltip: {
-            trigger: 'axis'
-          }
-          ,
-          legend: {
-            data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
-          }
-          ,
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          }
-          ,
-          toolbox: {
-            feature: {
-              saveAsImage: {}
-            }
-          }
-          ,
-          xAxis: {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-          }
-          ,
-          yAxis: {
-            type: 'value'
-          }
-          ,
-          series: [
-            {
-              name: '邮件营销',
-              type: 'line',
-              stack: '总量',
-              data: [120, 132, 101, 134, 90, 230, 210]
-            },
-            {
-              name: '联盟广告',
-              type: 'line',
-              stack: '总量',
-              data: [220, 182, 191, 234, 290, 330, 310]
-            },
-            {
-              name: '视频广告',
-              type: 'line',
-              stack: '总量',
-              data: [150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-              name: '直接访问',
-              type: 'line',
-              stack: '总量',
-              data: [320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-              name: '搜索引擎',
-              type: 'line',
-              stack: '总量',
-              data: [820, 932, 901, 934, 1290, 1330, 1320]
-            }
-          ]
-        }
+      return {
+        // 初始化空对象
+        chart: null,
+        // 初始化图表配置
+        opinion: ['高富帅', '矮富帅', '高富挫', '矮富挫', '女生'],
+        opinionData: [{
+          value: 26,
+          name: '高富帅'
+        }, {
+          value: 31,
+          name: '矮富帅'
+        }, {
+          value: 18,
+          name: '高富挫'
+        }, {
+          value: 28,
+          name: '矮富挫'
+        }, {
+          value: 21,
+          name: '女生'
+        }]
       }
     },
-    methods: {},
+    methods: {
+      // 绘图
+      drawGraph(id) {
+        // 绘图方法
+        this.chart = echarts.init(document.getElementById(id))
+        // 皮肤添加同一般使用方式
+        this.chart.showLoading()
+        // 返回到data中
+        var that = this
+        // ajax 请求数据
+        $.ajax({
+          // 方式
+          type: "GET",
+          // 是否异步
+          async: true,
+          // 路径||API
+          url: "xxx",
+          //返回数据形式为json
+          dataType: "json",
+          // 加载成功
+          success: function(result) {
+            // 更新初始数据
+            that.opinionData = result
+          },
+          // 加载错误
+          error: function(errorMsg) {
+            // alert("请求数据失败!");
+            console.log(errorMsg)
+          }
+        })
+        // set
+        this.chart.setOption({
+          title: {
+            text: '女生喜欢的男生种类',
+            subtext: '纯属扯犊子',
+            x: 'center'
+          },
+          tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+          },
+          legend: {
+            x: 'center',
+            y: 'bottom',
+            data: this.opinion // 别忘了this
+          },
+          toolbox: {
+            show: true,
+            feature: {
+              mark: {
+                show: true
+              },
+              dataView: {
+                show: true,
+                readOnly: false
+              },
+              magicType: {
+                show: true,
+                type: ['pie']
+              },
+              restore: {
+                show: true
+              },
+              saveAsImage: {
+                show: true
+              }
+            }
+          },
+          calculable: true,
+          series: [{
+            name: '种类',
+            type: 'pie',
+            // 内圆半径，外圆半径
+            radius: [30, 100],
+            // 位置，左右，上下
+            center: ['50%', '50%'],
+            roseType: 'area',
+            data: this.opinionData, // 别忘了this
+          }]
+        });
+        this.chart.hideLoading()
+      }
+    },
+    // keypoint：执行方法
+    // “将回调延迟到下次 DOM 更新循环之后执行。在修改数据之后立即使用它，然后等待 DOM 更新。”
+    created () {
+      this.$nextTick(function() {
+        this.drawGraph('main')
+      })
+    },
     components: {}
   }
 </script>
