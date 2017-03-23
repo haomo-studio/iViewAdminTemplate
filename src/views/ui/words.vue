fdf553a8ec447946b7ddb695aac5b2714853720c
yide <994144745@qq.com>
2017-03-23 22:17:22 +0800

    s

--- src/views/ui/words.vue
+++ src/views/ui/words.vue
@@ -1,5 +1,46 @@
+<style>
+    .wrap{padding:50px 20px;}
+ .h1{
+     margin-top:30px;
+ }
+ .anchor{
+     margin-top:15px;
+     font-weight: 300;
+ }
+ .cont{
+     margin-top: 15px;
+     font-size: 14px;
+     padding-right: 15px;
+ }
+ .tit1{
+     font-size: 16px;
+     margin-top: 20px;
+ }
+ .ul1 li{
+
+ }
+ .demo-font-img{
+     width: 100%;
+ }
+ .chinafont{
+     margin-top: 15px;
+     font-size: 14px;
+     margin-left: 25px;
+ }
+</style>
 <template>
-    文字
+    <div class="wrap">
+        <h1 _v-7946e36e="" class="h1">Font 字体</h1>
+        <div class="anchor" _v-7946e36e>
+            <h2>概述</h2>
+        </div>
+        <p _v-7946e36e="" class="cont">iView 的 CSS 对字体进行了统一规范，力求在不同平台、浏览器下能显示出其最佳的效果。我们推荐 <i _v-7946e36e="" class="ivu-icon ivu-icon-social-apple"></i><!--v-component--> macOS（iOS）优先的策略，在不支持苹方字体的情况，使用备用字体。</p>
+        <p class="tit1">字体预览</p>
+        <ul _v-7946e36e class="ul1">
+            <li _v-7946e36e class="chinafont">中文字体</li>
+            <img class="demo-font-img" src="http://v1.iviewui.com/dist/aac43973c6b6d72bf7aa852ecb2302c5.png" _v-7946e36e />
+        </ul>
+    </div>
 </template>
 <script>
     export default {
