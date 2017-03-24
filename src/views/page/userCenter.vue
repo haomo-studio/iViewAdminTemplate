a86caedb5609f96cac59ca87fa43033a6d625a8b
zhaoyaping <zhaoyaping@haomo-studio.com>
2017-03-24 12:53:03 +0800

    finsh

--- src/views/page/userCenter.vue
+++ src/views/page/userCenter.vue
@@ -1,122 +1,222 @@
 <template>
-    <div >
-        <img :src="person">
-    </div>
-    <div class="layout">
-        <Menu mode="horizontal" theme="dark" active-name="1">
-            <div class="layout-logo"></div>
-            <div class="layout-nav">
-                <Menu-item name="1">
-                    <Icon type="ios-navigate"></Icon>
-                    导航一
-                </Menu-item>
-                <Menu-item name="2">
-                    <Icon type="ios-keypad"></Icon>
-                    导航二
-                </Menu-item>
-                <Menu-item name="3">
-                    <Icon type="ios-analytics"></Icon>
-                    导航三
-                </Menu-item>
-                <Menu-item name="4">
-                    <Icon type="ios-paper"></Icon>
-                    导航四
-                </Menu-item>
+    <Tabs active-key="key1">
+        <Tab-pane label="标签一" key="key1">标签一的内容</Tab-pane>
+        <Tab-pane label="标签二" key="key2">标签二的内容</Tab-pane>
+        <Tab-pane label="标签三" key="key3">标签三的内容</Tab-pane>
+    </Tabs>
+    <div class="left">
+        <div class="left-1">
+            <i-input type="textarea" :rows="4" placeholder="请输入..."></i-input>
+            <div class="post">
+                <Icon type="image" class="icon"></Icon>
+                <Icon type="android-person-add" class="icon"></Icon>
+                <Icon type="ios-location" class="icon"></Icon>
+                <i-button type="primary" class="i-button">post</i-button>
             </div>
-        </Menu>
-        <Menu mode="horizontal" active-name="1">
-            <div class="layout-assistant">
-                <Menu-item name="1">二级导航</Menu-item>
-                <Menu-item name="2">二级导航</Menu-item>
-                <Menu-item name="3">二级导航</Menu-item>
+
+        </div>
+        <div class="left-2">
+            <div>
+                <img src="" alt="">
+                <div class="lf">
+                    Garry Newman posted on your timeline <br>32 minutes ago
+                </div>
+                <p>Remember the place we were talking about the other night? Found it!</p>
+                <div><img src="" alt=""></div>
+                <i-button type="text" size="small" style="margin:5px;">
+                    <Icon type="ios-heart-outline" style="font-size: 1em"></Icon>
+                    Like(5)
+                </i-button>
+                <i-button type="text" size="small" style="margin:5px;">
+                    <Icon type="android-share" style="font-size: 1em"></Icon>
+                    Share(21)
+                </i-button>
             </div>
-        </Menu>
-        <div class="layout-breadcrumb">
-            <Breadcrumb>
-                <Breadcrumb-item href="#">首页</Breadcrumb-item>
-                <Breadcrumb-item href="#">应用中心</Breadcrumb-item>
-                <Breadcrumb-item>某应用</Breadcrumb-item>
-            </Breadcrumb>
+            <div>
+                <div>1 comments</div>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                Alice Freeman June 10, 2015 <br> That’s a wonderful place. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend ligula. Fusce posuere in sapien ac facilisis. Etiam sit amet justo non felis ornare feugiat.
+                    </div>
+                    <a> Reply</a>
+                    <Icon type="ios-flag" style="margin:10px;vertical-align: middle"></Icon>
+                </p>
+            </div>
         </div>
-        <div class="layout-content">
-            内容区域
+        <div>
+            <img src="" alt="">
+            <i-input type="textarea" placeholder="Add a comment..."></i-input>
         </div>
-        <div class="layout-copy">
-            2011-2016 &copy; TalkingData
+            <i-button type="info"  style="margin:5px;">Post Comment</i-button>
+    </div>
+        <div class="left-3">
+            <div>
+                <img src="" alt="">
+                <div class="lf">
+                    Garry Newman posted on your timeline <br>32 minutes ago
+                </div>
+                <p>Remember the place we were talking about the other night? Found it!</p>
+                <div><img src="" alt=""></div>
+                <i-button type="text" size="small" style="margin:5px;">
+                    <Icon type="ios-heart-outline" style="font-size: 1em"></Icon>
+                    Like(5)
+                </i-button>
+                <i-button type="text" size="small" style="margin:5px;">
+                    <Icon type="android-share" style="font-size: 1em"></Icon>
+                    Share(21)
+                </i-button>
+            </div>
+            <div>
+                <div>1 comments</div>
+                <div>
+                    <img src="" alt="">
+                    <div>
+                        Alice Freeman June 10, 2015 <br> That’s a wonderful place. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend ligula. Fusce posuere in sapien ac facilisis. Etiam sit amet justo non felis ornare feugiat.
+                    </div>
+                    <a> Reply</a>
+                    <Icon type="ios-flag" style="margin:10px;vertical-align: middle"></Icon>
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="">
+                <i-input type="textarea" placeholder="Add a comment..."></i-input>
+            </div>
+            <i-button type="info"  style="margin:5px;">Post Comment</i-button>
+        </div>
+        <div class="left-4">
+            <img src="" alt="">
+            <div class="lf">
+                Carl Henderson shared something with you <br>
+                June 10, 2015
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et eleifend ligula. Fusce posuere in sapien ac facilisis.
+                Etiam sit amet justo non felis ornare feugiat. Aenean lorem ex, ultrices sit amet ligula sed...</p>
+                <i-button type="text" size="small" style="margin:5px;">
+                    <Icon type="ios-heart-outline" style="font-size: 1em"></Icon>
+                    Like(5)
+                </i-button>
+                <i-button type="text" size="small" style="margin:5px;">
+                    <Icon type="android-share" style="font-size: 1em"></Icon>
+                    Share(21)
+                </i-button>
+                <div>
+                    <img src="" alt="">
+                    <div><i-input type="textarea" placeholder="请输入..."></i-input>
+                        <i-button type="info">Post Comment</i-button></div>
+                </div>
+            </div>
+        </div>
+        <div class="left-5">
+            <div>
+                <img src="" alt="">
+                <p>
+                    Jane Dean shared a video with you <br>
+                    June 1, 2015
+                </p>
+            </div>
+            <img src="" alt="">
+            <p> Durak (Stop) - 2014 (Animated Short Film) from serdarcotuk on Vimeo.</p>
+            <i-button type="text" size="small" style="margin:5px;">
+                <Icon type="ios-heart-outline" style="font-size: 1em"></Icon>
+                Like(5)
+            </i-button>
+            <i-button type="text" size="small" style="margin:5px;">
+                <Icon type="android-share" style="font-size: 1em"></Icon>
+                Share(21)
+            </i-button>
+            <div>
+                <img src="" alt="">
+                <div>
+                    <i-input type="textarea" placeholder="请输入..."></i-input>
+                    <i-button type="info">Post Comment</i-button>
+                </div>
+            </div>
         </div>
     </div>
-    <!--<Row class="userCenter-top">-->
-        <!--<i-col span="4" >-->
-            <!--<Icon type="android-hangout" class="icon"></Icon>-->
-            <!--<Icon type="person" class="icon"></Icon>-->
-            <!--<Icon type="lightbulb" class="icon"></Icon>-->
-            <!--<Icon type="star" style="color:#ffb300;border-right:1px solid #ddd" class="icon"></Icon>-->
-        <!--</i-col>-->
-        <!--<i-col span="14">col-12</i-col>-->
-        <!--<i-col span="6">col-12</i-col>-->
-    <!--</Row>-->
+    <div class="right">
+        <i-col span="24">
+            <Card >
+                <p slot="title"  style="background-color:#039be5">
+                    <a href="" class="lf" style="color:#000"> Latest Activity</a>
+                    <a href="" class="rg" style="color:#000">See All</a>
+                </p>
+                <p>
+                     <img src="" alt="">
+                     <p>
+                       Alice Freeman started following you. <br>13 mins. ago
+                     </p>
+                 </p>
+                <p>
+                    <img src="" alt="">
+                <p>
+                    Alice Freeman started following you. <br>13 mins. ago
+                </p>
+                </p>
+                <p>
+                    <img src="" alt="">
+                <p>
+                    Alice Freeman started following you. <br>13 mins. ago
+                </p>
+                </p>
+                <p>
+                    <img src="" alt="">
+                <p>
+                    Alice Freeman started following you. <br>13 mins. ago
+                </p>
+                </p>
+                <p>
+                    <img src="" alt="">
+                <p>
+                    Alice Freeman started following you. <br>13 mins. ago
+                </p>
+                </p>
+                <p>
+                    <img src="" alt="">
+                <p>
+                    Alice Freeman started following you. <br>13 mins. ago
+                </p>
+                </p>
+            </Card>
+        </i-col>
+    </div>
 </template>
 <script>
     export default {
-      data(){
-          return {
-              person:'img/back-img.jpg'
-          }
 
-      }
     }
 </script>
 
 <style>
-    .layout{
-        border: 1px solid #d7dde4;
-        background: #f5f7f9;
+ .left{
+     float:left;
+     width:65%;
+ }
+ .post{
+     width:100%;
+     height:10%;
+     position:relative;
+     background-color: #f3f4f5;
+ }
+.right{
+    float:right;
+    width:30%;
+    margin-right: 2%;
+}
+.icon{
+    padding:15px;
+    font-size:2em;
+}
+ .i-button{
+     position: absolute;
+     top:20%;
+     right:2%;
     }
-    .layout-logo{
-        width: 100px;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        float: left;
-        position: relative;
-        top: 15px;
-        left: 20px;
-    }
-    .layout-nav{
-        width: 420px;
-        margin: 0 auto;
-    }
-    .layout-assistant{
-        width: 300px;
-        margin: 0 auto;
-        height: inherit;
-    }
-    .layout-breadcrumb{
-        padding: 10px 15px 0;
-    }
-    .layout-content{
-        min-height: 200px;
-        margin: 15px;
-        overflow: hidden;
-        background: #fff;
-        border-radius: 4px;
-    }
-    .layout-content-main{
-        padding: 10px;
-    }
-    .layout-copy{
-        text-align: center;
-        padding: 10px 0 20px;
-        color: #9ea7b4;
-    }
-   /*.userCenter-top{*/
-       /*position:fixed;*/
-       /*top:0;*/
-       /*left:0;*/
-       /*border:1px solid #ddd;*/
-   /*}*/
-    /*.icon{*/
-        /*font-size: 2em;*/
-        /*padding:4% 6%;*/
-    /*}*/
+ .left img{
+     float:left;
+ }
+.lf{
+    float:left;
+}
+.rg{float:right;}
 </style>
