556ed416632bf6844f69c1093ce631fa703a6234
fuhao <fuhao@haomo-studio.com>
2017-03-25 14:21:31 +0800

    code

--- src/views/ui/buttons.vue
+++ src/views/ui/buttons.vue
@@ -11,6 +11,7 @@
                <i-button type="success">成功按钮</i-button>
                <i-button type="warning">警告按钮</i-button>
                <i-button type="error">错误按钮</i-button>
+            <p></p>
         </div>
         <div class="example-button">
             <i-button type="primary" shape="circle" icon="ios-search"></i-button>
@@ -64,99 +65,202 @@
                 <span v-else>Loading...</span>
             </i-button>
         </div>
+
         <div class="example-button">
-            <h4>基本</h4>
-            <br><br>
-            <Button-group>
-                <i-button>取消</i-button>
-                <i-button type="primary">确定</i-button>
-            </Button-group>
-            <Button-group>
-                <i-button disabled>昨日</i-button>
-                <i-button disabled>今日</i-button>
-                <i-button disabled>明日</i-button>
-            </Button-group>
-            <Button-group>
-                <i-button type="primary">L</i-button>
-                <i-button>M</i-button>
-                <i-button type="ghost">M</i-button>
-                <i-button type="dashed">R</i-button>
-            </Button-group>
-            <br><br>
-            <h4>配合图标</h4>
-            <br><br>
-            <Button-group>
-                <i-button type="primary">
-                    <Icon type="chevron-left"></Icon>
-                    前进
-                </i-button>
-                <i-button type="primary">
-                    后退
-                    <Icon type="chevron-right"></Icon>
-                </i-button>
-            </Button-group>
-            <Button-group>
-                <i-button type="primary" icon="ios-skipbackward"></i-button>
-                <i-button type="primary" icon="ios-skipforward"></i-button>
-            </Button-group>
-            <Button-group>
-                <i-button type="ghost" icon="ios-color-wand-outline"></i-button>
-                <i-button type="ghost" icon="ios-sunny-outline"></i-button>
-                <i-button type="ghost" icon="ios-crop"></i-button>
-                <i-button type="ghost" icon="ios-color-filter-outline"></i-button>
-            </Button-group>
-            <br><br>
-            <h4>圆角</h4>
-            <br><br>
-            <Button-group shape="circle">
-                <i-button type="primary">
-                    <Icon type="chevron-left"></Icon>
-                    前进
-                </i-button>
-                <i-button type="primary">
-                    后退
-                    <Icon type="chevron-right"></Icon>
-                </i-button>
-            </Button-group>
-            <Button-group shape="circle">
-                <i-button type="primary" icon="ios-skipbackward"></i-button>
-                <i-button type="primary" icon="ios-skipforward"></i-button>
-            </Button-group>
-            <Button-group shape="circle">
-                <i-button type="ghost" icon="ios-color-wand-outline"></i-button>
-                <i-button type="ghost" icon="ios-sunny-outline"></i-button>
-                <i-button type="ghost" icon="ios-crop"></i-button>
-                <i-button type="ghost" icon="ios-color-filter-outline"></i-button>
-            </Button-group>
-            <br><br>
-            <h4>尺寸</h4>
-            <br><br>
-            <Button-group size="large">
-                <i-button type="ghost">Large</i-button>
-                <i-button type="ghost">Large</i-button>
-            </Button-group>
-            <Button-group>
-                <i-button type="ghost">Default</i-button>
-                <i-button type="ghost">Default</i-button>
-            </Button-group>
-            <Button-group size="small">
-                <i-button type="ghost">Small</i-button>
-                <i-button type="ghost">Small</i-button>
-            </Button-group>
-            <br><br>
-            <Button-group size="large" shape="circle">
-                <i-button type="ghost">Large</i-button>
-                <i-button type="ghost">Large</i-button>
-            </Button-group>
-            <Button-group shape="circle">
-                <i-button type="ghost">Default</i-button>
-                <i-button type="ghost">Default</i-button>
-            </Button-group>
-            <Button-group size="small" shape="circle">
-                <i-button type="ghost">Small</i-button>
-                <i-button type="ghost">Small</i-button>
-            </Button-group>
+            <div class="button-icon" style="float:left">
+                <h4>基本</h4>
+                <br><br>
+                <Button-group>
+                    <i-button>取消</i-button>
+                    <i-button type="primary">确定</i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button disabled>昨日</i-button>
+                    <i-button disabled>今日</i-button>
+                    <i-button disabled>明日</i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button type="primary">L</i-button>
+                    <i-button>M</i-button>
+                    <i-button type="ghost">M</i-button>
+                    <i-button type="dashed">R</i-button>
+                </Button-group>
+                <br><br>
+                <h4>配合图标</h4>
+                <br><br>
+                <Button-group>
+                    <i-button type="primary">
+                        <Icon type="chevron-left"></Icon>
+                        前进
+                    </i-button>
+                    <i-button type="primary">
+                        后退
+                        <Icon type="chevron-right"></Icon>
+                    </i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button type="primary" icon="ios-skipbackward"></i-button>
+                    <i-button type="primary" icon="ios-skipforward"></i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button type="ghost" icon="ios-color-wand-outline"></i-button>
+                    <i-button type="ghost" icon="ios-sunny-outline"></i-button>
+                    <i-button type="ghost" icon="ios-crop"></i-button>
+                    <i-button type="ghost" icon="ios-color-filter-outline"></i-button>
+                </Button-group>
+                <br><br>
+                <h4>圆角</h4>
+                <br><br>
+                <Button-group shape="circle">
+                    <i-button type="primary">
+                        <Icon type="chevron-left"></Icon>
+                        前进
+                    </i-button>
+                    <i-button type="primary">
+                        后退
+                        <Icon type="chevron-right"></Icon>
+                    </i-button>
+                </Button-group>
+                <Button-group shape="circle">
+                    <i-button type="primary" icon="ios-skipbackward"></i-button>
+                    <i-button type="primary" icon="ios-skipforward"></i-button>
+                </Button-group>
+                <Button-group shape="circle">
+                    <i-button type="ghost" icon="ios-color-wand-outline"></i-button>
+                    <i-button type="ghost" icon="ios-sunny-outline"></i-button>
+                    <i-button type="ghost" icon="ios-crop"></i-button>
+                    <i-button type="ghost" icon="ios-color-filter-outline"></i-button>
+                </Button-group>
+                <br><br>
+                <h4>尺寸</h4>
+                <br><br>
+                <Button-group size="large">
+                    <i-button type="ghost">Large</i-button>
+                    <i-button type="ghost">Large</i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button type="ghost">Default</i-button>
+                    <i-button type="ghost">Default</i-button>
+                </Button-group>
+                <Button-group size="small">
+                    <i-button type="ghost">Small</i-button>
+                    <i-button type="ghost">Small</i-button>
+                </Button-group>
+                <br><br>
+                <Button-group size="large" shape="circle">
+                    <i-button type="ghost">Large</i-button>
+                    <i-button type="ghost">Large</i-button>
+                </Button-group>
+                <Button-group shape="circle">
+                    <i-button type="ghost">Default</i-button>
+                    <i-button type="ghost">Default</i-button>
+                </Button-group>
+                <Button-group size="small" shape="circle">
+                    <i-button type="ghost">Small</i-button>
+                    <i-button type="ghost">Small</i-button>
+                </Button-group>
+            </div>
+            <div class="button-code" style="float:right;width:50%">
+                <pre>
+                    <code class="java">
+                        <template>
+                        <h4>基本</h4>
+                <br><br>
+                <Button-group>
+                    <i-button>取消</i-button>
+                    <i-button type="primary">确定</i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button disabled>昨日</i-button>
+                    <i-button disabled>今日</i-button>
+                    <i-button disabled>明日</i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button type="primary">L</i-button>
+                    <i-button>M</i-button>
+                    <i-button type="ghost">M</i-button>
+                    <i-button type="dashed">R</i-button>
+                </Button-group>
+                <br><br>
+                <h4>配合图标</h4>
+                <br><br>
+                <Button-group>
+                    <i-button type="primary">
+                        <Icon type="chevron-left"></Icon>
+                        前进
+                    </i-button>
+                    <i-button type="primary">
+                        后退
+                        <Icon type="chevron-right"></Icon>
+                    </i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button type="primary" icon="ios-skipbackward"></i-button>
+                    <i-button type="primary" icon="ios-skipforward"></i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button type="ghost" icon="ios-color-wand-outline"></i-button>
+                    <i-button type="ghost" icon="ios-sunny-outline"></i-button>
+                    <i-button type="ghost" icon="ios-crop"></i-button>
+                    <i-button type="ghost" icon="ios-color-filter-outline"></i-button>
+                </Button-group>
+                <br><br>
+                <h4>圆角</h4>
+                <br><br>
+                <Button-group shape="circle">
+                    <i-button type="primary">
+                        <Icon type="chevron-left"></Icon>
+                        前进
+                    </i-button>
+                    <i-button type="primary">
+                        后退
+                        <Icon type="chevron-right"></Icon>
+                    </i-button>
+                </Button-group>
+                <Button-group shape="circle">
+                    <i-button type="primary" icon="ios-skipbackward"></i-button>
+                    <i-button type="primary" icon="ios-skipforward"></i-button>
+                </Button-group>
+                <Button-group shape="circle">
+                    <i-button type="ghost" icon="ios-color-wand-outline"></i-button>
+                    <i-button type="ghost" icon="ios-sunny-outline"></i-button>
+                    <i-button type="ghost" icon="ios-crop"></i-button>
+                    <i-button type="ghost" icon="ios-color-filter-outline"></i-button>
+                </Button-group>
+                <br><br>
+                <h4>尺寸</h4>
+                <br><br>
+                <Button-group size="large">
+                    <i-button type="ghost">Large</i-button>
+                    <i-button type="ghost">Large</i-button>
+                </Button-group>
+                <Button-group>
+                    <i-button type="ghost">Default</i-button>
+                    <i-button type="ghost">Default</i-button>
+                </Button-group>
+                <Button-group size="small">
+                    <i-button type="ghost">Small</i-button>
+                    <i-button type="ghost">Small</i-button>
+                </Button-group>
+                <br><br>
+                <Button-group size="large" shape="circle">
+                    <i-button type="ghost">Large</i-button>
+                    <i-button type="ghost">Large</i-button>
+                </Button-group>
+                <Button-group shape="circle">
+                    <i-button type="ghost">Default</i-button>
+                    <i-button type="ghost">Default</i-button>
+                </Button-group>
+                <Button-group size="small" shape="circle">
+                    <i-button type="ghost">Small</i-button>
+                    <i-button type="ghost">Small</i-button>
+                </Button-group>
+                    </template>
+                    </code>
+                </pre>
+            </div>
         </div>
+
         <div class="example-button">
             <Button-group vertical>
                 <i-button type="ghost" icon="social-facebook"></i-button>
@@ -188,6 +292,16 @@
         .example-button{
             border-bottom: 2px solid #ddd;
             padding: 30px;
+        }
+        .example-button:after{
+            content:"";
+            display:block;
+            clear: both;
+        }
+        .example-button:before{
+            content:'';
+            display:block;
+            clear: both;
         }
         h4{
             display: inline-block;
